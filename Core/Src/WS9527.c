cd7314183472cd726313b593d159fe772b62cad6
tusindfryd <20538005+tusindfryd@users.noreply.github.com>
2022-01-05 19:42:16 +0100

    lerp the signal out
    this... this could actually work???

--- Core/Src/WS9527.c
+++ Core/Src/WS9527.c
@@ -2,6 +2,7 @@
 
 #include "WS9527.h"
 
+uint16_t WS9527_Reading_ADC;
 uint16_t WS9527_Reading;
 
 void WS9527_Initialize() {
@@ -13,12 +14,13 @@ void WS9527_StartReadings() {
 }
 
 void WS9527_GetReadings(ADC_HandleTypeDef* hadc) {
-	/* let's say that 100% is the reading of 2000 which *
-	 * then would mean that 1% is reading divided by 20 *
-	 *                                     (...roughly) */
-	if(hadc->Instance == ADC1) {
-		WS9527_Reading = HAL_ADC_GetValue(&hadc1) / 20;
-	    }
+	/* first we lerp everything out and then scale the result by 18  *
+	 * that's because maximally moist soil gives the reading of 1800 *
+	 * so 1% is the reading divided by 18                            */
+	if (hadc->Instance == ADC1) {
+		WS9527_Reading_ADC = (int)((1 - 0.001) * WS9527_Reading_ADC + 0.001 * HAL_ADC_GetValue(&hadc1));
+		WS9527_Reading = WS9527_Reading_ADC / 18;
+	}
 }
 
 
