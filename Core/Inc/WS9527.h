9540cd8af086aec054769e67ce89bc5c33b0402f
tusindfryd <20538005+tusindfryd@users.noreply.github.com>
2022-01-06 17:13:23 +0100

    refactor + autoformat

--- Core/Inc/WS9527.h
+++ Core/Inc/WS9527.h
@@ -3,8 +3,10 @@
 #endif
 
 #include <stdint.h>
+#include <float.h>
 #include "adc.h"
 
+float lerp(uint16_t v0, uint32_t v1, float t);
 void WS9527_Initialize();
 void WS9527_StartReadings();
-void WS9527_GetReadings(ADC_HandleTypeDef* hadc);
+void WS9527_GetReadings(ADC_HandleTypeDef *hadc);
--- Core/Src/WS9527.c
+++ Core/Src/WS9527.c
@@ -5,6 +5,10 @@
 uint16_t WS9527_Reading_ADC;
 uint16_t WS9527_Reading;
 
+float lerp(uint16_t v0, uint32_t v1, float t) {
+	return (1 - t) * v0 + t * v1;
+}
+
 void WS9527_Initialize() {
 	HAL_ADC_Start_IT(&hadc1);
 }
@@ -13,14 +17,14 @@ void WS9527_StartReadings() {
 	HAL_ADC_Start_IT(&hadc1);
 }
 
-void WS9527_GetReadings(ADC_HandleTypeDef* hadc) {
+void WS9527_GetReadings(ADC_HandleTypeDef *hadc) {
 	/* first we lerp everything out and then scale the result by 18  *
 	 * that's because maximally moist soil gives the reading of 1800 *
 	 * so 1% is the reading divided by 18                            */
 	if (hadc->Instance == ADC1) {
-		WS9527_Reading_ADC = (int)((1 - 0.001) * WS9527_Reading_ADC + 0.001 * HAL_ADC_GetValue(&hadc1));
+		WS9527_Reading_ADC = (int) lerp(WS9527_Reading_ADC,
+				HAL_ADC_GetValue(&hadc1), 0.001);
 		WS9527_Reading = WS9527_Reading_ADC / 18;
 	}
 }
-
 
